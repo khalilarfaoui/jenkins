e16e6f78cc207ab9b72b511285166faa751062aa
khalilarfaoui2 <khalilarfaoui791@gmail.com>
2024-12-25 09:26:23 +0100

    't'

--- src/main/java/com/example/demo/ProductService.java
+++ src/main/java/com/example/demo/ProductService.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,9 +11,19 @@ import java.util.Optional;
 
 @Service
 public class ProductService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
+    private final MeterRegistry meterRegistry;
+
     private List<Product> products = new ArrayList<>();
 
+    // Inject MeterRegistry via le constructeur
+    public ProductService(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
     public List<Product> getAllProducts() {
+        logger.warn("Get");
         return products;
     }
 
@@ -31,12 +44,25 @@ public class ProductService {
             product.setDescription(updatedProduct.getDescription());
             product.setPrice(updatedProduct.getPrice());
             return product;
+        } else {
+            // Log de l'avertissement
+            logger.warn("Update failed: Product with ID {} not found", id);
+
+            // Incrémenter un compteur pour l'avertissement
+            meterRegistry.counter("product.update.warn", "type", "not_found").increment();
+            return null;
         }
-        return null;
     }
 
     public boolean deleteProduct(Long id) {
-        return products.removeIf(p -> p.getId().equals(id));
+        boolean isRemoved = products.removeIf(p -> p.getId().equals(id));
+        if (!isRemoved) {
+            // Log de l'avertissement
+            logger.warn("Delete failed: Product with ID {} not found", id);
+
+            // Incrémenter un compteur pour l'avertissement
+            meterRegistry.counter("product.delete.warn", "type", "not_found").increment();
+        }
+        return isRemoved;
     }
 }
-
